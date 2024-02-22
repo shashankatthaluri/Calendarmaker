3057b54cf1ef0fdff8df2f1d98be6b6043bbea08
Shashank Atthaluri <26676223+shashankatthaluri@users.noreply.github.com>
2024-02-22 06:54:10 +0530

    Update calendarmaker.py

--- calendarmaker.py
+++ calendarmaker.py
@@ -1,14 +1,13 @@
 import datetime
 
 # Set up the constants:
-DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-        'Friday', 'Saturday')
-MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July',
-          'August', 'September', 'October', 'November', 'December')
+DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+MONTHS = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
 
 print('Calendar Maker')
 
-while True:  # Loop to get a year from the user.
+# Loop to get a year from the user.
+while True:
     print('Enter the year for the calendar:')
     response = input('> ')
 
@@ -16,80 +15,78 @@ while True:  # Loop to get a year from the user.
         year = int(response)
         break
 
-    print('Please enter a numeric year, like 2022.')
+    print('Please enter a number for the year, like 2022.')
     continue
 
-while True:  # Loop to get a month from the user.
+# Loop to get a month from the user.
+while True:
     print('Enter the month for the calendar, 1-12:')
     response = input('> ')
 
-    if not response.isdecimal():
-        print('Please enter a numeric month, like 3 for March.')
-        continue
-
-    month = int(response)
-    if 1 <= month <= 12:
-        break
+    if response.isdecimal():
+        month = int(response)
+        if 1 <= month <= 12:
+            break
 
     print('Please enter a number from 1 to 12.')
 
-
-def getCalendarFor(year, month):
-    calendarText = ''  # calendarText will contain the string of our calendar.
+# Function to create the calendar for the specified year and month.
+def get_calendar_for(year, month):
+    calendar_text = ''  # This will contain the string of our calendar.
 
     # Put the month and year at the top of the calendar:
-    calendarText += (' ' * 34) + MONTHS[month - 1] + ' ' + str(year) + '\n'
+    calendar_text += (' ' * 34) + MONTHS[month - 1] + ' ' + str(year) + '\n'
 
     # Add the days of the week labels to the calendar:
-    # (!) Try changing this to abbreviations: SUN, MON, TUE, etc.
-    calendarText += '...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..\n'
+    calendar_text += '...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..\n'
 
-    # The horizontal line string that separate weeks:
-    weekSeparator = ('+----------' * 7) + '+\n'
+    # The horizontal line string that separates weeks:
+    week_separator = ('+----------' * 7) + '+\n'
 
     # The blank rows have ten spaces in between the | day separators:
-    blankRow = ('|          ' * 7) + '|\n'
+    blank_row = ('|          ' * 7) + '|\n'
 
-    # Get the first date in the month. (The datetime module handles all
-    # the complicated calendar stuff for us here.)
-    currentDate = datetime.date(year, month, 1)
+    # Get the first date in the month.
+    current_date = datetime.date(year, month, 1)
 
-    # Roll back currentDate until it is Sunday. (weekday() returns 6
-    # for Sunday, not 0.)
-    while currentDate.weekday() != 6:
-        currentDate -= datetime.timedelta(days=1)
+    # Roll back current_date until it is Sunday.
+    while current_date.weekday() != 6:  # Sunday is represented as 6, not 0.
+        current_date -= datetime.timedelta(days=1)
 
-    while True:  # Loop over each week in the month.
-        calendarText += weekSeparator
+    # Loop over each week in the month.
+    while True:
+        calendar_text += week_separator
 
-        # dayNumberRow is the row with the day number labels:
-        dayNumberRow = ''
-        for i in range(7):
-            dayNumberLabel = str(currentDate.day).rjust(2)
-            dayNumberRow += '|' + dayNumberLabel + (' ' * 8)
-            currentDate += datetime.timedelta(days=1) # Go to next day.
-        dayNumberRow += '|\n'  # Add the vertical line after Saturday.
+        # day_number_row is the row with the day number labels:
+        day_number_row = ''
+        for _ in range(7):
+            day_number_label = str(current_date.day).rjust(2)
+            day_number_row += '|' + day_number_label + (' ' * 8)
+            current_date += datetime.timedelta(days=1)  # Go to the next day.
+        day_number_row += '|\n'  # Add the vertical line after Saturday.
 
         # Add the day number row and 3 blank rows to the calendar text.
-        calendarText += dayNumberRow
-        for i in range(3):  # (!) Try changing the 4 to a 5 or 10.
-            calendarText += blankRow
+        calendar_text += day_number_row
+        for _ in range(3):  # Add three blank rows.
+            calendar_text += blank_row
 
         # Check if we're done with the month:
-        if currentDate.month != month:
+        if current_date.month != month:
             break
 
     # Add the horizontal line at the very bottom of the calendar.
-    calendarText += weekSeparator
-    return calendarText
+    calendar_text += week_separator
+    return calendar_text
 
+# Get the calendar text for the specified year and month.
+calendar_text = get_calendar_for(year, month)
 
-calendarText = getCalendarFor(year, month)
-print(calendarText)  # Display the calendar.
+# Display the calendar.
+print(calendar_text)
 
 # Save the calendar to a text file:
-calendarFilename = 'calendar_{}_{}.txt'.format(year, month)
-with open(calendarFilename, 'w') as fileObj:
-    fileObj.write(calText)
+calendar_filename = 'calendar_{}_{}.txt'.format(year, month)
+with open(calendar_filename, 'w') as file_obj:
+    file_obj.write(calendar_text)
 
-print('Saved to ' + calendarFilename)
+print('Saved to ' + calendar_filename)
